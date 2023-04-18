6993f8dfa7c93f666253248150435e03863c1ccc
Talhaimran03 <101459540+Talhaimran03@users.noreply.github.com>
2023-04-18 19:39:18 +0200

    Update ModelRilevazioni.php

--- backend/model/ModelRilevazioni.php
+++ backend/model/ModelRilevazioni.php
@@ -1,10 +1,27 @@
 <?php
-require_once(__DIR__ . "\\..\\connect.php");
+require_once("../connect.php");
 
 class ModelRilevazioni {
   static function get_all() {
-    require("../connect.php");
+    global $conn;
     $query = "SELECT * FROM Rilevazioni";
+    $result = mysqli_query($conn, $query);
+    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $data;
+  }
+
+  static function get_by_city() {
+    global $conn;
+    $provincia = $_GET["city"];
+    $query = "SELECT codseqst FROM Stazioni WHERE provincia='$provincia'";
+    $result = mysqli_query($conn, $query);
+    $codseqst_array = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $codseqst = $row['codseqst'];
+        array_push($codseqst_array, $codseqst);
+    }
+
+    $query = "SELECT * FROM Rilevazioni WHERE codseqst IN ('" . implode("','", $codseqst_array) . "')";
     $result = mysqli_query($conn, $query);
     $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
     return $data;
