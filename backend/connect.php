76b32bf863e9e39357721f0c12faa1a08624f188
Talhaimran03 <101459540+Talhaimran03@users.noreply.github.com>
2023-04-18 19:42:23 +0200

    Update connect.php

--- backend/connect.php
+++ backend/connect.php
@@ -6,6 +6,7 @@ $dbname = "ARPAVGruppon";
 
 // Connection to mysql server
 $conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_set_charset($conn, "utf8");
 
 if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
