7863b0dbf07017afa04e410313003eace7d9e47f
Talhaimran03 <101459540+Talhaimran03@users.noreply.github.com>
2023-04-18 17:59:24 +0200

    Update ModelStazioni.php

--- backend/model/ModelStazioni.php
+++ backend/model/ModelStazioni.php
@@ -3,9 +3,10 @@ require_once(__DIR__ . "\\..\\connect.php");
 
 class ModelStazioni {
   static function get_all() {
-    return array(
-      "key" => "value",
-      "key1" => "value1"
-    );
+    require("../connect.php");
+    $query = "SELECT * FROM Stazioni";
+    $result = mysqli_query($conn, $query);
+    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $data;
   }
 }
